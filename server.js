019be1c2f394f3c86634c31043940ba10e305c1d
Summer Jade <summerjadefrosal@gmail.com>
2022-08-14 20:27:18 -0400

    deployed to heroku

--- server.js
+++ server.js
@@ -97,5 +97,6 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
-app.listen(process.env.PORT || 3000, 
+app.listen(process.env.PORT || PORT, 
 	() => console.log("Server is running... 🚀"));
+    
